ab43a7914a2b4518cdbd95a7b2f504328fcd1b67
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-01-14 12:56:31 +0200

    done

---
+++ index.js
@@ -0,0 +1,31 @@
+//1
+
+// const a = 10;
+// const b = 15;
+
+// console.log(a > b ? console.log(a) : console.log(b))
+
+//2
+
+// const year = +prompt("Введіть ваш рік народження")
+
+// alert("You are" + " " + (2023 - year))
+
+//3
+
+const name1 = "Igor";
+const name2 = "Oleg";
+const name3 = "Olena";
+const name4 = "Nastya";
+
+switch(name1){
+    case "Igor":
+        console.log("male");
+    case "Oleg":
+        console.log("male");
+    case "Olena":
+        console.log("female");
+    case "Nastya":
+        console.log("female");
+}
+
