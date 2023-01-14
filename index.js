3d46079e09fa77291b9008accf6c490511e65cfc
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-01-14 12:23:09 +0200

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+//1
+
+// const a = 100000;
+// const b = 100000000;
+
+// console.log(a.toString().length + b.toString().length)
+
+//2
+
+// const a = "Hello World"
+
+// console.log(a.slice(6,10) + " " + a.slice(0,5))
+
+//3
+
+const a = "Test string, .., -- string test"
+
+console.log(a.slice(0,11) + " " + a.slice(20,31))
