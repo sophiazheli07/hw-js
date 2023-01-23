812264b3221edb57066a3f369f21c8bbaf69b8e4
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-01-23 11:56:17 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//1
+
+// const data = {
+// name: true,
+// age: 12,
+// country: "Ukraine"
+// };
+
+// for (let key in data) {
+//     if(typeof data.age == 'number'){
+//         delete data.age
+//     }
+//     else(typeof data.name == 'boolean')
+//         delete data.name;
+// };
+// console.log(data)
+
+
+//2
+
+// const data1 = {n: 1, m: 2};
+// const data2 = {a: 1, b: 2, c: 3};
+
+// const first = (Object.keys(data1).length);
+// const second = (Object.keys(data2).length);
+
+// if(first > second){
+//     alert("data1" + " is longer");
+// } else alert("data2" + " is longer");
+
