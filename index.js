3c431e3cb819afb9ef9c5d00ad641b6014a31566
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-01-27 11:05:53 +0200

    done

--- index.js
+++ index.js
@@ -3,4 +3,22 @@
 const arr = ["1", "2", "4", "7"];
 const res = [];
 
-for(let el )
+arr.forEach((el) => {
+    res.push(+el)
+})
+console.log(res);
+
+// 2
+
+const arr1 = ["Poland", "Ukraine", "USA"];
+const res1 = [];
+
+arr1.forEach((el) => {
+    res1.push(el[0])
+});
+console.log(res1);
+
+// 3 
+
+const arr2 = ["Lviv", "Kyiv", "Poltava"];
+console.log(arr2.indexOf("Kyiv"));
