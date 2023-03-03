555851e7ea527584478e742b13996c53cf1bf034
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-03-03 20:47:30 +0200

    commit-one

---
+++ index.js
@@ -0,0 +1,27 @@
+const rateButtonOne = document.querySelector("#rate-one");
+// rateButtonTwo = document.querySelector("#rate-two");
+// rateButtonThree = document.querySelector("#rate-three");
+// rateButtonFour = document.querySelector("#rate-four");
+// rateButtonFive = document.querySelector("#rate-five");
+// rateButtonSix = document.querySelector("#rate-six");
+// rateButtonSeven = document.querySelector("#rate-seven");
+// rateButtonEight = document.querySelector("#rate-eight");
+// rateButtonNine = document.querySelector("#rate-nine");
+// rateButtonTen = document.querySelector("#rate-ten");
+
+// allRateButtons = document.querySelectorAll("button");
+
+// const Rates = [];
+rateButtonOne.onclick = () => {
+    alert("!")
+}
+
+// rateButtonOne.onclick = () => {
+//     // const rate = rate.value
+//     // console.log(rate);
+
+//     // const ante = {rate};
+//     // Rates.push(ante);
+// }
+
+// console.log(Rates)
